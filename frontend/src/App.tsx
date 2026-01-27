2097288b7a104e29f2b57a3709d29c2f14826ecd
LogicMason <33540416+LogicMason5@users.noreply.github.com>
2026-01-27 23:27:39 +0900

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,38 +1,32 @@
-import { useState } from 'react';
-import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { routes } from "./routes";
 import { Navbar, Footer } from "./widgets/layout";
-import { StockNews, CryptoNews } from './pages';
-
+import { StockNews, CryptoNews } from "./pages";
 
 function App() {
-  const { pathname } = useLocation();
-
   return (
     <>
       <Navbar />
 
       <main className="pt-0">
         <Routes>
-          {routes.map(({ path, element }, key) => (
-            <Route
-              key={key}
-              path={path}
-              element={element}
-            />
+          {/* Main routes */}
+          {routes.map(({ path, element }, index) => (
+            <Route key={index} path={path} element={element} />
           ))}
 
-
+          {/* News routes */}
           <Route path="/news">
             <Route path="stock_news" element={<StockNews />} />
             <Route path="crypto_news" element={<CryptoNews />} />
           </Route>
 
-          {/* Default route */}
+          {/* Fallback */}
           <Route path="*" element={<Navigate to="/home" replace />} />
         </Routes>
-        <Footer />
       </main>
+
+      <Footer />
     </>
   );
 }
